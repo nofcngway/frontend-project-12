31d822ae2ed31d328516f18f8ddb8df3514c9571
nofcngway <nofcngway@gmail.com>
2026-01-27 17:27:41 +0500

    step 11: work in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,32 +5,42 @@ import MainPage from "./pages/MainPage.jsx";
 import Header from "./components/Header/Header.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
 import { ToastContainer } from "react-toastify";
+import { Provider, ErrorBoundary } from '@rollbar/react'
+
+const rollbarConfig = {
+  accessToken: '92a7459130b4451eb3ed25f385435763',
+  environment: 'testenv',
+};
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="d-flex flex-column h-100">
-        <Header />
-        <Routes>
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<MainPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </div>
-    </BrowserRouter>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <div className="d-flex flex-column h-100">
+            <Header />
+            <Routes>
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/" element={<MainPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick={false}
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
+          </div>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </Provider>
   )
 }
 
